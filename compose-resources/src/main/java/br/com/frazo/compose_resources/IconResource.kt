a6143fa904ffbbfdc97631f6579d4130d4900b06
Ygor Frazão <ygorluizfrazao@gmail.com>
2023-03-31 09:18:19 -0300

    Initial commit

---
+++ compose-resources/src/main/java/br/com/frazo/compose_resources/IconResource.kt
@@ -0,0 +1,54 @@
+package br.com.frazo.compose_resources
+
+import android.annotation.SuppressLint
+import androidx.annotation.DrawableRes
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.painterResource
+
+class IconResource private constructor(
+    @DrawableRes private val resID: Int?,
+    private val imageVector: ImageVector?,
+    val contentDescription: String
+) {
+
+    @Composable
+    fun asPainterResource(): Painter {
+        resID?.let {
+            return painterResource(id = resID)
+        }
+        return rememberVectorPainter(image = imageVector!!)
+    }
+
+    @Composable
+    fun ComposeIcon(modifier: Modifier = Modifier) {
+        Icon(
+            modifier = modifier,
+            painter = asPainterResource(),
+            contentDescription = contentDescription
+        )
+    }
+
+    companion object {
+        @JvmStatic
+        @SuppressLint
+        fun fromDrawableResource(
+            @DrawableRes resID: Int,
+            contentDescription: String = ""
+        ): IconResource {
+            return IconResource(resID, null, contentDescription)
+        }
+
+        @JvmStatic
+        fun fromImageVector(
+            imageVector: ImageVector?,
+            contentDescription: String = ""
+        ): IconResource {
+            return IconResource(null, imageVector, contentDescription)
+        }
+    }
+}
---
+++ compose-resources/src/main/java/br/com/frazo/compose_resources/TextResource.kt
@@ -0,0 +1,34 @@
+package br.com.frazo.compose_resources
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+
+sealed class TextResource {
+
+    data class RuntimeString(val value: String) : TextResource()
+
+    class StringResource(
+        @StringRes val resId: Int,
+        vararg val args: Any
+    ) : TextResource()
+
+
+    @Composable
+    fun asString(): String {
+        return when (this) {
+            is RuntimeString -> value
+            is StringResource -> stringResource(id = resId, *args)
+        }
+    }
+
+    @SuppressLint
+    fun asString(context: Context): String {
+        return when (this) {
+            is RuntimeString -> value
+            is StringResource -> context.getString(resId, *args)
+        }
+    }
+}
